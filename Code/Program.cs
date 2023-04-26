c65726f4697b36f9f04a0c3c413f235353a6fff8
Nano1412 <nano.khongsri@gmail.com>
2023-04-26 15:52:35 +0700

    add comment

--- Code/Program.cs
+++ Code/Program.cs
@@ -2,82 +2,79 @@
 
 class Program{
   static void Main(string[] args){
+    // initial variables
     int n = int.Parse(Console.ReadLine());
     n++;
-    int counter = 0;
-    char[] space = new char[n];
+    char[] slot = new char[n];
     for(int i = 0; i < n; i++){
-      space[i] = '_';
+      slot[i] = '_';
     }
-    bool isend = true;
+    bool isnotend = true;
 
-    while(isend){
-      int firstnum = int.Parse(Console.ReadLine());
-      int secondnum = int.Parse(Console.ReadLine());
+    while(isnotend){
+      // loop input
+      int num1 = int.Parse(Console.ReadLine());
+      int num2 = int.Parse(Console.ReadLine());
 
-      bool first = false;
-      bool second = false;
-      bool isplace1 = false;
-      bool isplace2 = false;
+      // declear condition booleen and reset once reach again
+      bool num1_valid = false;
+      bool num2_valid = false;
+      bool num1_isplace = false;
+      bool num2_isplace = false;
       bool carslot = true;
 
-      if(firstnum < 0){
-        firstnum = 0;
-        first = true;
-      }
-      if(space[firstnum] != 'X'){
-        first = true;
-        isplace1 = true;
-      } 
-
-      if(secondnum < 0){
-        secondnum = 0;
-        second = true;
-      }
-      if(space[secondnum] != 'X'){
-        second = true;
-        isplace2 = true;
-      }
+      // check if the slot are empty and check if num1 and num2 are being place
+      Input(ref num1,ref num1_valid, ref num1_isplace, slot[num1]);
+      Input(ref num2,ref num2_valid, ref num2_isplace, slot[num2]);
       
-      
-      counter = 0;
-      if(isplace1){counter--;}
-      if(isplace2){counter--;}
-
+      // calculate how many slot left if num1 and num2 are being place before actually place them on to the slot
+      int counter = 0;
+      if(num1_isplace){counter--;}
+      if(num2_isplace){counter--;}
       for(int i = 0; i < n; i++){
-      if (space[i] != 'X'){
-        counter++;
+        if (slot[i] != 'X'){
+          counter++;
+        }
       }
-    }
-      if(counter == 1){
-        Console.WriteLine("All stall are reserved.");
-        isend = false;
+
+      // Condition
+      if(!num1_valid || !num2_valid) {
+        Console.WriteLine("The stall is reserved.");
       }
 
       if(counter < 1){
         Console.WriteLine("The entrance can’t be reserved.");
-        isend = true;
         carslot = false;
       }
 
-
-
-      if(first && second && carslot){
-        space[firstnum] = 'X';
-        space[secondnum] = 'X';
-        Print(space,n);
-      } 
-      if((!first || !second) && carslot) {
-        Console.WriteLine("The stall is reserved.");
+      // End condition
+      if(counter == 1){
+        Console.WriteLine("All stall are reserved.");
+        isnotend = false;
       }
 
-      
+      // actually place num1 and num2 on to the array after every condition are checked and print it out
+      if(num1_valid && num2_valid && carslot){
+        slot[num1] = 'X';
+        slot[num2] = 'X';
+        Print(slot,n);
+      } 
     }
   }
+  static void Input(ref int num,ref bool valid, ref bool isplace, char isnotX){
+    if(num < 0){
+      num = 0;
+      valid = true;
+    }
+    if(isnotX != 'X'){
+      valid = true;
+      isplace = true;
+    } 
+  }
 
-  static void Print(char[] space,int n){
+  static void Print(char[] slot,int n){
     for(int i = 1; i < n; i++){
-      Console.Write("{0} ",space[i]);
+      Console.Write("{0} ",slot[i]);
     }
     Console.WriteLine("");
   }
